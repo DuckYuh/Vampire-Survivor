48b02588a0969bd10483e29813184d3693609e1e
DuckYuh <huynguyen531pro@gmail.com>
2025-06-25 20:10:57 +0700

    add attack effect

--- Scripts/Enemies/enemy.gd
+++ Scripts/Enemies/enemy.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 
 @onready var anin = get_node("AnimatedSprite2D") 
-@onready var player = get_node("../Char1")
 
 const SPEED = 100.0
 
 func _physics_process(delta: float) -> void:
+	var player = get_nearest_player()
 	var direction = (player.position - position).normalized()
 	velocity = direction * SPEED
 	
@@ -17,3 +17,11 @@ func _physics_process(delta: float) -> void:
 		anin.scale.x = 1
 
 	move_and_slide()
+
+func get_nearest_player() -> Node:
+	var players = get_tree().get_nodes_in_group("players")
+	var nearest: Node = null
+
+	for p in players:
+		nearest = p
+	return nearest
